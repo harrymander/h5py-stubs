3ad147702720ed0811c676ab5e685b593b7af270
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2025-09-05 16:34:18 +1200

    Add some basic tests for Group.get type annotations

---
+++ tests/test_group_get.py
@@ -0,0 +1,121 @@
+import textwrap
+
+import mypy.api
+
+
+def assert_group_get_return_type(exp_type: str, *calls_args: str) -> None:
+    __tracebackhide__ = True
+
+    if not calls_args:
+        raise TypeError("got 1 positional argument, 2 or more required")
+
+    preamble = """\
+    from typing import assert_type
+    import h5py
+    group: h5py.Group
+    """
+    lines = [
+        textwrap.dedent(preamble),
+        *(f"assert_type(group.get({args}), {exp_type})" for args in calls_args),
+    ]
+    stdout, stderr, ret = mypy.api.run(["-c", "\n".join(lines)])
+    if ret == 0:
+        return
+
+    # TODO: parse output to find specific errors
+    stdout = textwrap.indent(stdout, "  ")
+    stderr = textwrap.indent(stderr, "  ")
+    msg = f"mypy failed ({ret = })\n{stdout}\n{stderr}"
+    raise AssertionError(msg)
+
+
+def test_group_get_no_default() -> None:
+    assert_group_get_return_type("h5py.HLObject | None", '"hello, world"')
+
+
+def test_group_get_with_default() -> None:
+    assert_group_get_return_type(
+        "int | h5py.HLObject",
+        '"hello, world", 42',
+        '"hello, world", default=42',
+    )
+
+
+def test_group_get_getclass_no_default() -> None:
+    assert_group_get_return_type(
+        "type[h5py.HLObject] | None",
+        '"hello, world", getclass=True',
+        '"hello, world", None, getclass=True',
+        '"hello, world", None, True',
+        '"hello, world", default=None, getclass=True',
+        '"hello, world", getclass=True, default=None',
+        '"hello, world", None, True, False',
+        '"hello, world", getclass=True, getlink=False',
+        '"hello, world", getlink=False, getclass=True',
+    )
+
+
+def test_group_get_getclass_with_default() -> None:
+    assert_group_get_return_type(
+        "type[h5py.HLObject] | int",
+        '"hello, world", 42, True',
+        '"hello, world", 42, getclass=True',
+        '"hello, world", default=42, getclass=True',
+        '"hello, world", getclass=True, default=42',
+        '"hello, world", 42, True, False',
+        '"hello, world", 42, getclass=True, getlink=False',
+        '"hello, world", 42, getlink=False, getclass=True',
+        '"hello, world", default=42, getlink=False, getclass=True',
+        '"hello, world", default=42, getclass=True, getlink=False',
+    )
+
+
+def test_group_get_getlink_no_default() -> None:
+    assert_group_get_return_type(
+        "h5py.HardLink | h5py.ExternalLink | h5py.SoftLink | None",
+        '"hello, world", getlink=True',
+        '"hello, world", None, getlink=True',
+        '"hello, world", None, False, True',
+        '"hello, world", default=None, getlink=True',
+        '"hello, world", getlink=True, default=None',
+        '"hello, world", getlink=True, getclass=False',
+        '"hello, world", getclass=False, getlink=True',
+        '"hello, world", default=None, getclass=False, getlink=True',
+        '"hello, world", default=None, getlink=True, getclass=False',
+    )
+
+
+def test_group_get_getlink_with_default() -> None:
+    assert_group_get_return_type(
+        "h5py.HardLink | h5py.ExternalLink | h5py.SoftLink | int",
+        '"hello, world", 42, getclass=False, getlink=True',
+        '"hello, world", 42, getlink=True, getclass=False',
+        '"hello, world", 42, False, True',
+        '"hello, world", default=42, getlink=True',
+        '"hello, world", getlink=True, default=42',
+        '"hello, world", 42, getclass=False, getlink=True',
+        '"hello, world", 42, getlink=True, getclass=False',
+        '"hello, world", default=42, getlink=True, getclass=False',
+        '"hello, world", default=42, getclass=False, getlink=True',
+    )
+
+
+def test_group_get_getclass_and_getlink_no_default() -> None:
+    assert_group_get_return_type(
+        "type[h5py.HardLink] | type[h5py.ExternalLink] | type[h5py.SoftLink] | None",
+        '"hello, world", getclass=True, getlink=True',
+        '"hello, world", getlink=True, getclass=True',
+        '"hello, world", None, getlink=True, getclass=True',
+        '"hello, world", default=None, getlink=True, getclass=True',
+        '"hello, world", None, True, True',
+    )
+
+
+def test_group_get_getclass_and_getlink_with_default() -> None:
+    assert_group_get_return_type(
+        "type[h5py.HardLink] | type[h5py.ExternalLink] | type[h5py.SoftLink] | int",
+        '"hello, world", 42, getclass=True, getlink=True',
+        '"hello, world", 42, getlink=True, getclass=True',
+        '"hello, world", getlink=True, getclass=True, default=42',
+        '"hello, world", 42, True, True',
+    )
