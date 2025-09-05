dba80bf70f8236025923b6839aff5cbff99404e8
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2025-09-05 14:50:10 +1200

    Add __all__ back to __init__

--- src/h5py-stubs/__init__.pyi
+++ src/h5py-stubs/__init__.pyi
@@ -2,58 +2,109 @@
 # See https://typing.python.org/en/latest/guides/writing_stubs.html#all
 
 from . import (
-    h5a,  # pyright: ignore[reportUnusedImport]
-    h5d,  # pyright: ignore[reportUnusedImport]
-    h5ds,  # pyright: ignore[reportUnusedImport]
-    h5f,  # pyright: ignore[reportUnusedImport]
-    h5fd,  # pyright: ignore[reportUnusedImport]
-    h5g,  # pyright: ignore[reportUnusedImport]
-    h5p,  # pyright: ignore[reportUnusedImport]
-    h5pl,  # pyright: ignore[reportUnusedImport]
-    h5r,  # pyright: ignore[reportUnusedImport]
-    h5s,  # pyright: ignore[reportUnusedImport]
-    h5t,  # pyright: ignore[reportUnusedImport]
-    h5z,  # pyright: ignore[reportUnusedImport]
-    version,  # pyright: ignore[reportUnusedImport]
+    h5a,
+    h5d,
+    h5ds,
+    h5f,
+    h5fd,
+    h5g,
+    h5p,
+    h5pl,
+    h5r,
+    h5s,
+    h5t,
+    h5z,
+    version,
 )
-from ._hl import filters  # pyright: ignore[reportUnusedImport]
-from ._hl.attrs import AttributeManager  # pyright: ignore[reportUnusedImport]
-from ._hl.base import Empty, HLObject, is_hdf5  # pyright: ignore[reportUnusedImport]
-from ._hl.dataset import Dataset  # pyright: ignore[reportUnusedImport]
-from ._hl.datatype import Datatype  # pyright: ignore[reportUnusedImport]
+from ._hl import filters
+from ._hl.attrs import AttributeManager
+from ._hl.base import Empty, HLObject, is_hdf5
+from ._hl.dataset import Dataset
+from ._hl.datatype import Datatype
 from ._hl.files import (
-    File,  # pyright: ignore[reportUnusedImport]
-    register_driver,  # pyright: ignore[reportUnusedImport]
-    registered_drivers,  # pyright: ignore[reportUnusedImport]
-    unregister_driver,  # pyright: ignore[reportUnusedImport]
+    File,
+    register_driver,
+    registered_drivers,
+    unregister_driver,
 )
 from ._hl.group import (
-    ExternalLink,  # pyright: ignore[reportUnusedImport]
-    Group,  # pyright: ignore[reportUnusedImport]
-    HardLink,  # pyright: ignore[reportUnusedImport]
-    SoftLink,  # pyright: ignore[reportUnusedImport]
+    ExternalLink,
+    Group,
+    HardLink,
+    SoftLink,
 )
-from ._hl.vds import VirtualLayout, VirtualSource  # pyright: ignore[reportUnusedImport]
-from ._selector import MultiBlockSlice  # pyright: ignore[reportUnusedImport]
-from .h5 import get_config  # pyright: ignore[reportUnusedImport]
-from .h5r import Reference, RegionReference  # pyright: ignore[reportUnusedImport]
-from .h5s import UNLIMITED  # pyright: ignore[reportUnusedImport]
+from ._hl.vds import VirtualLayout, VirtualSource
+from ._selector import MultiBlockSlice
+from .h5 import get_config
+from .h5r import Reference, RegionReference
+from .h5s import UNLIMITED
 from .h5t import (
-    check_dtype,  # pyright: ignore[reportUnusedImport]
-    check_enum_dtype,  # pyright: ignore[reportUnusedImport]
-    check_opaque_dtype,  # pyright: ignore[reportUnusedImport]
-    check_ref_dtype,  # pyright: ignore[reportUnusedImport]
-    check_string_dtype,  # pyright: ignore[reportUnusedImport]
-    check_vlen_dtype,  # pyright: ignore[reportUnusedImport]
-    enum_dtype,  # pyright: ignore[reportUnusedImport]
-    opaque_dtype,  # pyright: ignore[reportUnusedImport]
-    ref_dtype,  # pyright: ignore[reportUnusedImport]
-    regionref_dtype,  # pyright: ignore[reportUnusedImport]
-    special_dtype,  # pyright: ignore[reportUnusedImport]
-    string_dtype,  # pyright: ignore[reportUnusedImport]
-    vlen_dtype,  # pyright: ignore[reportUnusedImport]
+    check_dtype,
+    check_enum_dtype,
+    check_opaque_dtype,
+    check_ref_dtype,
+    check_string_dtype,
+    check_vlen_dtype,
+    enum_dtype,
+    opaque_dtype,
+    ref_dtype,
+    regionref_dtype,
+    special_dtype,
+    string_dtype,
+    vlen_dtype,
 )
 from .version import version as __version__
+
+__all__ = [
+    "UNLIMITED",
+    "AttributeManager",
+    "Dataset",
+    "Datatype",
+    "Empty",
+    "ExternalLink",
+    "File",
+    "Group",
+    "HLObject",
+    "HardLink",
+    "MultiBlockSlice",
+    "Reference",
+    "RegionReference",
+    "SoftLink",
+    "VirtualLayout",
+    "VirtualSource",
+    "check_dtype",
+    "check_enum_dtype",
+    "check_opaque_dtype",
+    "check_ref_dtype",
+    "check_string_dtype",
+    "check_vlen_dtype",
+    "enum_dtype",
+    "filters",
+    "get_config",
+    "h5a",
+    "h5d",
+    "h5ds",
+    "h5f",
+    "h5fd",
+    "h5g",
+    "h5p",
+    "h5pl",
+    "h5r",
+    "h5s",
+    "h5t",
+    "h5z",
+    "is_hdf5",
+    "opaque_dtype",
+    "ref_dtype",
+    "regionref_dtype",
+    "register_driver",
+    "registered_drivers",
+    "special_dtype",
+    "string_dtype",
+    "unregister_driver",
+    "version",
+    "vlen_dtype",
+]
 
 def enable_ipython_completer() -> None: ...
 def run_tests(args: str = ...) -> int: ...
