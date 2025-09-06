a26bde52e46807eea0fb3283404aa1808d9519a1
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2025-09-06 13:31:31 +1200

    Add type annotations for Dataset.regionref

--- tests/test_references.py
+++ tests/test_references.py
@@ -38,3 +38,45 @@ def test_indexing_group_with_parameterized_ref_returns_narrowed_object(obj_type:
     ref: h5py.Reference[h5py.{obj_type}]
     assert_type(group[ref], h5py.{obj_type})
     """)
+
+
+@pytest.mark.parametrize("dataset_dtype", ["Any", "np.float64"])
+def test_indexing_group_with_dataset_regionref_returns_dataset(dataset_dtype: str):
+    assert_mypy_passes(f"""\
+    from typing import assert_type, Any
+    import h5py
+    import numpy as np
+
+    group: h5py.Group
+    dataset: h5py.Dataset[{dataset_dtype}]
+    region = dataset.regionref[:10]
+    assert_type(group[region], h5py.Dataset[{dataset_dtype}])
+    """)
+
+
+def test_indexing_group_with_hlobject_regionref_returns_hlobject() -> None:
+    assert_mypy_passes("""\
+    from typing import assert_type
+    import h5py
+
+    group: h5py.Group
+    object: h5py.HLObject
+    region = object.regionref[:10]
+    assert_type(group[region], h5py.HLObject)
+    """)
+
+
+def test_code_unreachable_after_indexing_group_regionref() -> None:
+    """
+    All HLObjects return a _RegionProxy from regionref property, but all proxy objects
+    except those returned from Dataset.regionref will fail with TypeError if indexed.
+    """
+    assert_mypy_passes("""
+    from typing import assert_never, assert_type
+    import h5py
+
+    group: h5py.Group
+    regionref = group.regionref
+    assert_type(regionref, "h5py._hl.base._RegionProxy[h5py.Group]")
+    assert_never(regionref[:10])
+    """)
