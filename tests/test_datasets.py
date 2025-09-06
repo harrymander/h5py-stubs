fd6c43f727bc7877ea652d8bc92525c2e550e718
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2025-09-06 15:22:45 +1200

    Test create_dataset works with bytes

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -18,6 +18,10 @@ def assert_mypy_passes_with_group(source: str) -> None:
     assert_mypy_passes(f"{textwrap.dedent(setup)}\n{textwrap.dedent(source)}")
 
 
+def parametrize_bytes_prefix() -> pytest.MarkDecorator:
+    return pytest.mark.parametrize("b", ["b", ""])
+
+
 @pytest.mark.parametrize(
     "args_expr",
     [
@@ -26,77 +30,81 @@ def assert_mypy_passes_with_group(source: str) -> None:
         "dtype=int",
     ],
 )
-def test_create_dataset_with_unknown_type_returns_any_dataset(args_expr: str):
+@parametrize_bytes_prefix()
+def test_create_dataset_with_unknown_type_returns_any_dataset(b: str, args_expr: str):
     assert_mypy_passes(f"""\
     from typing import assert_type, Any
     import h5py
     import numpy as np
     group: h5py.Group
-    dataset = group.create_dataset("name", {args_expr})
+    dataset = group.create_dataset({b}"name", {args_expr})
     assert_type(dataset, h5py.Dataset[Any])
     """)
 
 
-def test_create_dataset_with_data_of_known_type_returns_typed_dataset() -> None:
-    assert_mypy_passes("""\
+@parametrize_bytes_prefix()
+def test_create_dataset_with_data_of_known_type_returns_typed_dataset(b: str) -> None:
+    assert_mypy_passes(f"""\
     from typing import assert_type
     import h5py
     import numpy as np
     group: h5py.Group
     data = np.array([1, 2, 3], dtype=np.float32)
-    dataset = group.create_dataset("name", data=data)
+    dataset = group.create_dataset({b}"name", data=data)
     assert_type(dataset, h5py.Dataset[np.float32])
     """)
 
 
-def test_create_dataset_with_untyped_data_and_typed_dtype_returns_typed_dataset() -> (
-    None
-):
-    assert_mypy_passes("""\
+@parametrize_bytes_prefix()
+def test_create_dataset_with_untyped_data_and_typed_dtype_returns_typed_dataset(b: str):
+    assert_mypy_passes(f"""\
     from typing import assert_type
     import h5py
     import numpy as np
     group: h5py.Group
-    dataset = group.create_dataset("name", data=[1, 2, 3], dtype=np.float32)
+    dataset = group.create_dataset({b}"name", data=[1, 2, 3], dtype=np.float32)
     assert_type(dataset, h5py.Dataset[np.float32])
     """)
 
 
+@parametrize_bytes_prefix()
 @pytest.mark.parametrize(
     "dtype,exp_type",
     [("np.uint16", "np.uint16"), ("int", "Any")],
 )
-def test_create_dataset_dtype_overrides_type_of_data(dtype: str, exp_type: str):
+def test_create_dataset_dtype_overrides_type_of_data(b: str, dtype: str, exp_type: str):
     assert_mypy_passes(f"""\
     from typing import assert_type, Any
     import h5py
     import numpy as np
     data = np.array([1, 2, 3], dtype=np.float32)
     group: h5py.Group
-    dataset = group.create_dataset("name", data=data, dtype={dtype})
+    dataset = group.create_dataset({b}"name", data=data, dtype={dtype})
     assert_type(dataset, h5py.Dataset[{exp_type}])
     """)
 
 
-def test_create_dataset_with_dtype_numpy_generic_class_returns_typed_dataset() -> None:
-    assert_mypy_passes("""\
+@parametrize_bytes_prefix()
+def test_create_dataset_with_dtype_numpy_generic_class_returns_typed_dataset(b: str):
+    assert_mypy_passes(f"""\
     from typing import assert_type
     import h5py
     import numpy as np
     group: h5py.Group
-    dataset = group.create_dataset("name", dtype=np.uint8)
+    dataset = group.create_dataset({b}"name", dtype=np.uint8)
     assert_type(dataset, h5py.Dataset[np.uint8])
     """)
 
 
-def test_create_dataset_with_dtype_object_returns_typed_dataset() -> None:
-    assert_mypy_passes("""\
+@parametrize_bytes_prefix()
+def test_create_dataset_with_dtype_object_returns_typed_dataset(b: str):
+    assert_mypy_passes(f"""\
     from typing import assert_type
     import h5py
     import numpy as np
     dtype: np.dtype[np.float64]
     group: h5py.Group
-    dataset = group.create_dataset("name", dtype=dtype)
+    dataset = group.create_dataset({b}"name", dtype=dtype)
     assert_type(dataset, h5py.Dataset[np.float64])
     """)
 
