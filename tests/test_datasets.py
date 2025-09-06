42a97ca4de5157fb65c8cac2a3dd52a38cd3ef11
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2025-09-06 15:41:45 +1200

    tests: add some tests for create_dataset_like

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,7 +2,7 @@ import textwrap
 
 import pytest
 
-from .mypy_assertions import assert_mypy_passes
+from .mypy_assertions import assert_mypy_fails, assert_mypy_passes
 
 
 def assert_mypy_passes_with_group(source: str) -> None:
@@ -107,6 +107,121 @@ def test_create_dataset_with_dtype_object_returns_typed_dataset(b: str):
     dataset = group.create_dataset({b}"name", dtype=dtype)
     assert_type(dataset, h5py.Dataset[np.float64])
     """)
+
+
+@parametrize_bytes_prefix()
+@pytest.mark.parametrize(
+    "extra_args",
+    [
+        "",
+        "dtype=int",
+        "data=[1, 2, 3]",
+        "data=np.array([1, 2, 3], dtype=np.float32)",
+    ],
+)
+def test_create_dataset_like_untyped_dataset_returns_untyped_dataset(
+    b: str,
+    extra_args: str,
+):
+    assert_mypy_passes(f"""\
+    from typing import assert_type, Any
+    import h5py
+    import numpy as np
+    dataset: h5py.Dataset
+    group: h5py.Group
+    new = group.create_dataset_like({b}"name", dataset, {extra_args})
+    assert_type(new, h5py.Dataset[Any])
+    """)
+
+
+@parametrize_bytes_prefix()
+@pytest.mark.parametrize(
+    "extra_args",
+    [
+        "",
+        "data=[1, 2, 3]",
+        "shape=(100,)",
+        # dtype of original dataset is kept unless dtype explicitly passed:
+        "data=[1.2, 2.2, 3.2]",
+        "data=np.array([1, 2, 3], dtype=np.float32)",
+    ],
+)
+def test_create_dataset_like_typed_dataset_returns_dataset_of_same_type(
+    b: str,
+    extra_args: str,
+):
+    assert_mypy_passes(f"""\
+    from typing import assert_type
+    import h5py
+    import numpy as np
+    dataset: h5py.Dataset[np.uint32]
+    group: h5py.Group
+    new = group.create_dataset_like({b}"name", dataset, {extra_args})
+    assert_type(new, h5py.Dataset[np.uint32])
+    """)
+
+
+@parametrize_bytes_prefix()
+@pytest.mark.parametrize("dataset_type", ["np.uint32", "Any"])
+@pytest.mark.parametrize(
+    "dtype,exp_type",
+    [
+        ("np.float32", "np.float32"),
+        ("float", "Any"),
+    ],
+)
+def test_create_dataset_like_with_dtype_overrides_type_of_original_dataset(
+    dataset_type: str,
+    dtype: str,
+    exp_type: str,
+    b: str,
+):
+    assert_mypy_passes(f"""\
+    from typing import assert_type, Any
+    import h5py
+    import numpy as np
+    dataset: h5py.Dataset[{dataset_type}]
+    group: h5py.Group
+    new = group.create_dataset_like({b}"name", dataset, dtype={dtype})
+    assert_type(new, h5py.Dataset[{exp_type}])
+    """)
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        '"name"',  # name only
+        '"name", "name2"',  # two strings
+        '"name", dataset, 100',  # three positional args
+    ],
+)
+def test_create_dataset_like_invalid_arguments_fails(args: str) -> None:
+    assert_mypy_fails(
+        f"""\
+        import h5py
+        dataset: h5py.Dataset
+        group: h5py.Group
+        group.create_dataset_like({args})
+        """,
+        4,
+        'No overload variant of "create_dataset_like"',
+    )
+
+
+def test_create_dataset_like_without_arguments_fails() -> None:
+    assert_mypy_fails(
+        """\
+        import h5py
+        dataset: h5py.Dataset
+        group: h5py.Group
+        group.create_dataset_like()
+        """,
+        4,
+        (
+            'All overload variants of "create_dataset_like" of "Group" '
+            "require at least one argument"
+        ),
+    )
 
 
 def test_index_typed_dataset_returns_typed_ndarray() -> None:
